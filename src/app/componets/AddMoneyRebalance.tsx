c8b0f8aaaa9dcea45a2241a31f0b8ad197c48a13
OginoHayase <ogihaya1214@icloud.com>
2025-09-19 12:09:02 +0900

    大枠のコンポーネントを作った

---
+++ src/app/componets/AddMoneyRebalance.tsx
@@ -0,0 +1,3 @@
+export default function AddMoneyRebalance() {
+  return <div>AddMoneyRebalance</div>;
+}
---
+++ src/app/componets/CalculationResult.tsx
@@ -0,0 +1,3 @@
+export default function CalculationResult() {
+  return <div>CalculationResult</div>;
+}
