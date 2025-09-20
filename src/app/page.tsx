573032407526ab402fe6f9c33c181d63dd2a4dfd
OginoHayase <ogihaya1214@icloud.com>
2025-09-21 01:17:31 +0900

    peopleを親のコンポーネントで管理するようにした。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
+import { useState } from "react";
+import { Person } from "@/app/types/deta";
 import AddPeople from "@/app/componets/AddPeople";
 import AddMoneyRebalance from "@/app/componets/AddMoneyRebalance";
 import CalculationResult from "@/app/componets/CalculationResult";
 
 export default function Home() {
+  const [people, setPeople] = useState<Person[]>([]);
   return (
     <div>
-      <AddPeople />
-      <AddMoneyRebalance />
+      <AddPeople people={people} setPeople={setPeople} />
+      <AddMoneyRebalance people={people} />
       <CalculationResult />
     </div>
   );
